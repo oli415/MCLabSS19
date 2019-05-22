a024f18801f7dfddde636c9c25fb55e736352239
Oliver Tobias Kreuzer <oliver.kreuzer@gmx.at>
2019-05-22 09:23:40 +0200

    added walls to Floor

--- app/src/main/java/com/example/activitymonitoring/Floor.java
+++ app/src/main/java/com/example/activitymonitoring/Floor.java
@@ -22,6 +22,18 @@ public class Floor {
 
         //add walls
         walls = new ArrayList<Line>();
-        
+        walls.add(new Line(new Position(8.98, 0.11), new Position(14.37, 0.11)));    //A
+        walls.add(new Line(new Position(8.98, 0.11), new Position(8.98, 16.73)));    //B
+        walls.add(new Line(new Position(14.37, 0.11), new Position(14.37, 10.1)));    //C
+        walls.add(new Line(new Position(10.21, 10.1), new Position(14.37, 10.1)));    //D
+        walls.add(new Line(new Position(10.21, 10.1), new Position(10.21, 16.73)));    //E
+        walls.add(new Line(new Position(10.21, 16.73), new Position(14.7, 16.73)));    //F
+        walls.add(new Line(new Position(14.7, 16.73), new Position(14.7, 18.97)));    //G
+        walls.add(new Line(new Position(10.21, 18.97), new Position(14.7, 18.97)));    //H
+        walls.add(new Line(new Position(0, 16.73), new Position(8.98, 16.73)));    //I
+        walls.add(new Line(new Position(0, 16.73), new Position(0, 18.97)));    //J
+        walls.add(new Line(new Position(0, 18.97), new Position(8.98, 18.97)));    //K
+        walls.add(new Line(new Position(8.98, 18.97), new Position(8.98, 44.56)));    //L
+        walls.add(new Line(new Position(10.21, 18.97), new Position(10.21, 46.47)));    //M
     }
 }
